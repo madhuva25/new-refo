e85764015d70f24a86acc9a0a99b23e0efc2b8c8
madhuva25 <127526196+madhuva25@users.noreply.github.com>
2025-10-23 01:40:21 +0530

    Update About.jsx
    removed

--- src/comp/About.jsx
+++ src/comp/About.jsx
@@ -7,7 +7,7 @@ function About() {
     <h3 className="text-4xl font-bold text-center mb-10">About Me</h3>
     <div className={`${COLORS.cardBg} p-8 rounded-xl shadow-2xl`}>
         <p className="text-[#a0aec0] leading-relaxed text-lg">
-            As an Aspiring Software Engineer and third-year MIS (Special) undergraduate, I focus on delivering clean, maintainable, and effective software solutions. My expertise spans **full-stack development**, with a core focus on **MERN Stack** (React.js, Node.js, Express.js, MongoDB) and **Enterprise Java** technologies (Spring Boot, Java EE). I have practical experience in integrating RESTful APIs and implementing secure authentication using JWT and Spring Security. I am passionate about system architecture, continuously learning modern development practices, and solving real-world problems through technology.
+            As an Aspiring Software Engineer and third-year MIS (Special) undergraduate, I focus on delivering clean, maintainable, and effective software solutions. My expertise spans full-stack development, with a core focus on MERN Stack (React.js, Node.js, Express.js, MongoDB) and **Enterprise Java technologies (Spring Boot, Java EE). I have practical experience in integrating RESTful APIs and implementing secure authentication using JWT and Spring Security. I am passionate about system architecture, continuously learning modern development practices, and solving real-world problems through technology.
         </p>
     </div>
 </section></div>
